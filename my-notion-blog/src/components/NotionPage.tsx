50c4d77e3e6db37ec9f384aaa8198765fa103ede
Youngsuk Kim <youngsuk.kim@learnship.com>
2026-02-23 17:40:44 +0900

    feat: Add code block rendering and syntax highlighting to Notion pages.

--- my-notion-blog/src/components/NotionPage.tsx
+++ my-notion-blog/src/components/NotionPage.tsx
@@ -1,8 +1,16 @@
 'use client'
 
+import dynamic from 'next/dynamic'
 import { NotionRenderer } from 'react-notion-x'
 import { ExtendedRecordMap } from 'notion-types'
 import 'react-notion-x/src/styles.css' // 필수 스타일
+import 'prismjs/themes/prism-tomorrow.css' // 코드 구문 강조 테마
+
+// 코드 블록 컴포넌트 (prismjs 사용으로 SSR 비활성화 필요)
+const Code = dynamic(
+    () => import('react-notion-x/build/third-party/code').then((m) => m.Code),
+    { ssr: false }
+)
 
 interface NotionPageProps {
     recordMap: ExtendedRecordMap
@@ -18,6 +26,9 @@ export default function NotionPage({ recordMap }: NotionPageProps) {
             recordMap={recordMap}
             fullPage={true}
             darkMode={false}
+            components={{
+                Code,
+            }}
         />
     )
 }
